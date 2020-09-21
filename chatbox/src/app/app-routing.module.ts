6e6d4c3383d82541510cc9deb5157beaea00c7f7
Gaganpreet Singh <gaganpreetsingh@griffithuni.edu.au>
2020-09-21 15:09:12 +1000

    Geneertaing new components for chat and users

--- chatbox/src/app/app-routing.module.ts
+++ chatbox/src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
+import { ChatInterfaceComponent } from './chat-interface/chat-interface.component';
+import { UserComponent } from './user/user.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
 
-
-const routes: Routes = [ {
- path: 'login', component: LoginComponent
-
-}];
+const appRoutes: Routes = [{
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+}, {
+    path: 'login',
+    component: LoginComponent
+}, {
+    path: 'user',
+    component: UserComponent,
+}, {
+    path: 'user/new',
+    component: CreateUserComponent
+}, {
+    path: 'chat',
+    component: ChatInterfaceComponent
+},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class AppRoutingModule { }
--- chatbox/src/app/app.module.ts
+++ chatbox/src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ChatService } from './services/chat.service';
+import { LoginService } from './services/login.service';
+import { UserService } from './services/user.service';
+import { ChatInterfaceComponent } from './chat-interface/chat-interface.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { CreateUserComponent } from './user/create-user/create-user.component';
+
+const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    ChatInterfaceComponent,
+    LoginComponent,
+    
+    UserComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    UserService,
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
